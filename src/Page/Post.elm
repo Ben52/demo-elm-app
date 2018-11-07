77ea88ce2120fb1f91be358b4b73370afab67adf
Binyomin Greenes <bygreenes@gmail.com>
2018-11-07 16:19:09 -0500

    wip

--- src/Page/Post.elm
+++ src/Page/Post.elm
@@ -6,7 +6,7 @@ import Api.Object
 import Api.Object.Post
 import Api.Query
 import Api.Scalar
-import Graphql.Field
+import Graphql.Field exposing (Field(..))
 import Graphql.Http exposing (Error, send)
 import Graphql.Operation exposing (RootQuery)
 import Graphql.SelectionSet exposing (SelectionSet(..), with)
@@ -25,16 +25,45 @@ type Msg
     | Fetch
 
 
+type Id
+    = Id String
+
+
+type Title
+    = Title String
+
+
+type Published
+    = Published Bool
+
+
+type alias Post =
+    { id : Id, title : Title, published : Published }
+
+
+type alias Response =
+    { publishedPosts : List Post }
+
+
+titleString : Post -> String
+titleString post =
+    let
+        (Title title) =
+            post.title
+    in
+    title
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( Model [], send GotResponse queryRequest )
+    ( Model [], send GotResponse postsRequest )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Fetch ->
-            ( model, send GotResponse queryRequest )
+            ( model, send GotResponse postsRequest )
 
         GotResponse res ->
             case res of
@@ -53,24 +82,26 @@ view : Model -> Html Msg
 view model =
     div [] <|
         button [ onClick Fetch ] [ text "Click me!" ]
-            :: List.map (\post -> div [] [ text post.title ]) model.posts
+            :: List.map (\post -> div [] [ text <| titleString post ]) model.posts
 
 
-queryRequest : Graphql.Http.Request Response
-queryRequest =
-    Graphql.Http.queryRequest "http://localhost:4000" query
+graphqlEndpoint : String
+graphqlEndpoint =
+    "http://localhost:4000"
 
 
-type alias Post =
-    { id : String, title : String, published : Bool }
+graphqlRequest : SelectionSet decodesTo RootQuery -> Graphql.Http.Request decodesTo
+graphqlRequest query =
+    Graphql.Http.queryRequest graphqlEndpoint query
 
 
-type alias Response =
-    { publishedPosts : List Post }
+postsRequest : Graphql.Http.Request Response
+postsRequest =
+    graphqlRequest postsQuery
 
 
-query : SelectionSet Response RootQuery
-query =
+postsQuery : SelectionSet Response RootQuery
+postsQuery =
     Api.Query.selection Response
         |> with (Api.Query.publishedPosts postSelection)
 
@@ -79,17 +110,20 @@ postSelection : SelectionSet Post Api.Object.Post
 postSelection =
     Api.Object.Post.selection Post
         |> with idField
-        |> with (titleField |> Graphql.Field.map String.toUpper)
+        |> with titleField
         |> with publishedField
 
 
+idField : Field Id Api.Object.Post
 idField =
-    Api.Object.Post.id |> Graphql.Field.map (\(Api.Scalar.Id id) -> id)
+    Api.Object.Post.id |> Graphql.Field.map (\(Api.Scalar.Id id) -> Id id)
 
 
+titleField : Field Title Api.Object.Post
 titleField =
-    Api.Object.Post.title
+    Api.Object.Post.title |> Graphql.Field.map (Title << String.toUpper)
 
 
+publishedField : Field Published Api.Object.Post
 publishedField =
-    Api.Object.Post.published
+    Api.Object.Post.published |> Graphql.Field.map Published
